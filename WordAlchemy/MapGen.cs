44a3edba8c6c2c0fb02e38899b7c4a668f996b50
Alex E.T. Snyder <alexetsnyder@gmail.com>
2023-08-28 09:52:19 -0400

    Update MapGen.cs
    Basic outline of a naïve way to make rivers.

--- WordAlchemy/MapGen.cs
+++ WordAlchemy/MapGen.cs
@@ -1,5 +1,6 @@
 ﻿
 using SDL2;
+using System.Diagnostics;
 using WordAlchemy.Helpers;
 
 namespace WordAlchemy
@@ -30,6 +31,7 @@ namespace WordAlchemy
         public void GenerateMap()
         {
             GenerateHeightMap();
+            //GenerateRivers();
         }
 
         private void GenerateHeightMap()
@@ -40,10 +42,91 @@ namespace WordAlchemy
                 {
                     float noise = Noise.GetNoise(i, j);
                     float remapNoise = MathHelper.Remap(noise, -1.0f, 1.0f, 0.0f, 100.0f);
+                    float fallOffValue = MathHelper.SigmoidFallOffMapCircular(j, i, Cols, Rows);
 
-                    HeightMap[i * Cols + j] = remapNoise * MathHelper.SigmoidFallOffMapCircular(j, i, Cols, Rows);
+                    HeightMap[i * Cols + j] = remapNoise * fallOffValue;
                 }
             }
+        }
+
+        private void GenerateRivers()
+        {
+            int[] riverStarts = GetMaxHeights(6);
+            
+            foreach (int index in riverStarts)
+            {
+                int j = index % Cols;
+                int i = (index - j) / Cols;
+
+                if (j > Cols / 2)
+                {
+                    j++;
+                    if (j < Cols)
+                    {
+                        int newIndex = i * Cols + j;
+                        float height = HeightMap[newIndex];
+
+                        while (height >= 20.0f && j < Cols)
+                        {
+                            if (height < 60.0f)
+                            {
+                                HeightMap[newIndex] = 19.0f;
+                            }                 
+
+                            j++;
+                            newIndex = i * Cols + j;
+                            height = HeightMap[newIndex];
+                        }
+                    }            
+                }
+                else
+                {
+                    j--;
+                    if (j >= 0)
+                    {
+                        int newIndex = i * Cols + j;
+                        float height = HeightMap[newIndex];
+
+                        while (height >= 20.0f && j >= 0)
+                        {
+                            if (height < 60.0f)
+                            {
+                                HeightMap[newIndex] = 19.0f;
+                            }
+
+                            j--;
+                            newIndex = i * Cols + j;
+                            height = HeightMap[newIndex];
+                        }
+                    }
+                }
+            }
+        }
+
+        private int[] GetMaxHeights(int n = 1)
+        {
+            float[] maxValues = new float[n];
+            int[] maxIndexes = new int[n];
+
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Cols; j++)
+                {
+                    float heightValue = HeightMap[i * Cols + j];
+
+                    for (int k = 0; k < n; k++)
+                    {
+                        if (heightValue > maxValues[k])
+                        {
+                            maxValues[k] = heightValue;
+                            maxIndexes[k] = i * Cols + j;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return maxIndexes;
         }
 
         public TerrainInfo GetTerrain(int i, int j)
